d75b7a1c17f34fb83284d02683c913e547bc744b
Karl Behrens <karlbehrensg@gmail.com>
2022-05-17 21:14:09 -0400

    Allocate and deallocate test

--- 01-Chapter/test_barches.py
+++ 01-Chapter/test_barches.py
@@ -8,3 +8,32 @@ def test_allocating_to_a_batch_reduces_the_available_quantity():
     batch.allocate(line)
 
     assert batch.available_quantity == 18
+
+def make_batch_and_line(sku, batch_qty, line_qty):
+    return (
+        Batch("batch-001", sku, batch_qty, eta=date.today()),
+        OrderLine("order-123", sku, line_qty)
+    )
+
+def test_can_allocate_if_available_greater_than_required():
+    large_batch, small_line = make_batch_and_line("ELEGANT-LAMP", 20, 2)
+    assert large_batch.can_allocate(small_line)
+
+def test_cannot_allocate_if_available_smaller_than_required():
+    small_batch, large_line = make_batch_and_line("ELEGANT-LAMP", 2, 20)
+    assert not small_batch.can_allocate(large_line)
+
+def test_can_allocate_if_available_equal_to_required():
+    batch, line = make_batch_and_line("ELEGANT-LAMP", 2, 2)
+    assert batch.can_allocate(line)
+
+def test_cannot_allocate_if_skus_do_not_match():
+    batch = Batch("batch-001", "UNCOMFORTABLE-CHAIR", 100, eta=None)
+    different_sku_lin = OrderLine("order-123", "EXPENSIVE-TOASTER", 10)
+    assert not batch.can_allocate(different_sku_lin)
+
+def test_can_only_deallocate_allocated_lines():
+    batch, unallocated_line = make_batch_and_line("DECORATIVE-TRINKET", 20, 2)
+    batch.deallocate(unallocated_line)
+    assert batch.available_quantity == 20
+ 
