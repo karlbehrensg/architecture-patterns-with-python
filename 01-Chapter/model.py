3fe25409ab8e750f9cdf16dcf3aa1a2f27e68fa1
Karl Behrens <karlbehrensg@gmail.com>
2022-05-21 14:56:50 -0400

    Domain Modeling

--- 01-Chapter/model.py
+++ 01-Chapter/model.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import Optional, List, Set
 
+
+class OutOfStock(Exception):
+    pass
 
 @dataclass(frozen=True)
 class OrderLine:
@@ -13,6 +16,7 @@ class Batch:
     def __init__(self, ref: str, sku: str, qty: int, eta: Optional[date] = None):
         self.reference = ref
         self.sku = sku
+        self.eta = eta
         self._purchased_quantity = qty
         self._allocations = set()
 
@@ -34,3 +38,29 @@ class Batch:
 
     def can_allocate(self, line: OrderLine) -> bool:
         return self.sku == line.sku and self.available_quantity >= line.qty
+
+    def __eq__(self, other):
+        if not isinstance(other, Batch):
+            return False
+        return other.reference == self.reference
+
+    def __hash__(self):
+        return hash(self.reference)
+
+    def __gt__(self, other):
+        if self.eta is None:
+            return False
+        if other.eta is None:
+            return True
+        return self.eta > other.eta
+
+
+def allocate(line: OrderLine, batches: List[Batch]) -> str:
+    try:
+        batch = next(
+            b for b in sorted(batches) if b.can_allocate(line)
+        )
+        batch.allocate(line)
+        return batch.reference
+    except StopIteration:
+        raise OutOfStock(f"Out of stock for sku {line.sku}")
