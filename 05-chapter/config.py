6b2716ba06e770c2c6be30f4247f4bd578f1ad25
Karl Behrens <karlbehrensg@gmail.com>
2022-05-23 20:58:53 -0400

    Initial structure for chapter 5

---
+++ 05-chapter/config.py
@@ -0,0 +1,16 @@
+import os
+
+FLASK_PORT = os.environ.get("PORT", 5005)
+
+def get_postgres_uri():
+    host = os.environ.get("DB_HOST", "localhost")
+    port = 5432 if host == "localhost" else 5432
+    password = os.environ.get("DB_PASSWORD", "abc123")
+    user, db_name = "allocation", "allocation"
+    return f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+
+
+def get_api_url():
+    host = os.environ.get("API_HOST", "localhost")
+    port = FLASK_PORT if host == "localhost" else 80
+    return f"http://{host}:{port}"
