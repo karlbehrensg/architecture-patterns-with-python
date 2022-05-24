af3eac6fbf165cff666d661db9e3e1e998fab23b
Karl Behrens <karlbehrensg@gmail.com>
2022-05-23 23:34:13 -0400

    TDD in High Gear and Low Gear

--- 05-chapter/entrypoints/flask_app.py
+++ 05-chapter/entrypoints/flask_app.py
@@ -1,5 +1,6 @@
 import sys
 import os
+from datetime import datetime
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
@@ -24,16 +25,31 @@ app = Flask(__name__)
 def allocate_endpoint():
     session = get_session()
     repo = repository.SqlAlchemyRepository(session)
-    line = model.OrderLine(
-        request.json["orderid"], request.json["sku"], request.json["qty"],
-    )
+    orderid, sku, qty = request.json["orderid"], request.json["sku"], request.json["qty"]
 
     try:
-        batchref = services.allocate(line, repo, session)
+        batchref = services.allocate(orderid, sku, qty, repo, session)
     except (model.OutOfStock, services.InvalidSku) as e:
         return {"message": str(e)}, 400
 
     return {"batchref": batchref}, 201
+
+
+@app.route("/add_batch", methods=["POST"])
+def add_batch():
+    session = get_session()
+    repo = repository.SqlAlchemyRepository(session)
+    eta = request.json['eta']
+
+    if eta is not None:
+        eta = datetime.fromisoformat(eta).date()
+    
+    services.add_batch(
+        request.json['ref'], request.json['sku'], request.json['qty'], eta, repo, session
+    )
+
+    return "OK", 201
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=config.FLASK_PORT)
--- 05-chapter/tests/unit/test_services.py
+++ 05-chapter/tests/unit/test_services.py
@@ -1,6 +1,10 @@
 from datetime import date, timedelta
+import uuid
 
 import pytest
+import requests
+
+import config
 from domain import model
 from adapters import repository
 from service_layer import services
@@ -9,6 +13,22 @@ from service_layer import services
 today = date.today()
 tomorrow = today + timedelta(days=1)
 later = tomorrow + timedelta(days=10)
+
+
+def random_suffix():
+    return uuid.uuid4().hex[:6]
+
+
+def random_sku(name=""):
+    return f"sku-{name}-{random_suffix()}"
+
+
+def random_batchref(name=""):
+    return f"batch-{name}-{random_suffix()}"
+
+
+def random_orderid(name=""):
+    return f"order-{name}-{random_suffix()}"
 
 
 class FakeRepository(repository.AbstractRepository):
@@ -24,6 +44,12 @@ class FakeRepository(repository.AbstractRepository):
     def list(self):
         return list(self._batches)
 
+    @staticmethod
+    def for_batch(ref, sku, qty, eta=None): 
+        return FakeRepository([
+            model.Batch(ref, sku, qty, eta),
+        ])
+
 
 class FakeSession:
     committed = False
@@ -32,31 +58,35 @@ class FakeSession:
         self.committed = True
 
 
-def test_returns_allocation():
-    line = model.OrderLine("o1", "COMPLICATED-LAMP", 10)
-    batch = model.Batch("b1", "COMPLICATED-LAMP", 100, eta=None)
-    repo = FakeRepository([batch])
+def post_to_add_batch(ref, sku, qty, eta):
+    url = config.get_api_url()
+    r = requests.post(
+        f'{url}/add_batch',
+        json={'ref': ref, 'sku': sku, 'qty': qty, 'eta': eta},
+    )
+    assert r.status_code == 201
 
-    result = services.allocate(line, repo, FakeSession())
-    assert result == "b1"
+
+def test_returns_allocation():
+    repo = FakeRepository.for_batch("batch1", "COMPLICATED-LAMP", 100, eta=None) 
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, FakeSession()) 
+    assert result == "batch1"
 
 
 def test_error_for_invalid_sku():
-    line = model.OrderLine("o1", "NONEXISTENTSKU", 10)
     batch = model.Batch("b1", "AREALSKU", 100, eta=None)
     repo = FakeRepository([batch])
 
     with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"):
-        services.allocate(line, repo, FakeSession())
+        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
 
 
 def test_commits():
-    line = model.OrderLine("o1", "OMINOUS-MIRROR", 10)
     batch = model.Batch("b1", "OMINOUS-MIRROR", 100, eta=None)
     repo = FakeRepository([batch])
     session = FakeSession()
 
-    services.allocate(line, repo, session)
+    services.allocate("o1", "OMINOUS-MIRROR", 10, repo, session)
     assert session.committed is True
 
 
@@ -76,7 +106,49 @@ def test_prefers_warehouse_batches_to_shipments():
     shipment_batch = model.Batch("shipment-batch", "RETRO-CLOCK", 100, eta=tomorrow) 
     repo = FakeRepository([in_stock_batch, shipment_batch])
     session = FakeSession()
-    line = model.OrderLine('oref', "RETRO-CLOCK", 10)
-    services.allocate(line, repo, session)
+    services.allocate('oref', "RETRO-CLOCK", 10, repo, session)
     assert in_stock_batch.available_quantity == 90
     assert shipment_batch.available_quantity == 100
+
+
+def test_add_batch():
+    repo, session = FakeRepository([]), FakeSession() 
+    services.add_batch("b1", "CRUNCHY-ARMCHAIR", 100, None, repo, session) 
+    assert repo.get("b1") is not None
+    assert session.committed
+
+
+def test_allocate_returns_allocation():
+    repo, session = FakeRepository([]), FakeSession() 
+    services.add_batch("batch1", "COMPLICATED-LAMP", 100, None, repo, session) 
+    result = services.allocate("o1", "COMPLICATED-LAMP", 10, repo, session) 
+    assert result == "batch1"
+
+
+def test_allocate_errors_for_invalid_sku():
+    repo, session = FakeRepository([]), FakeSession() 
+    services.add_batch("b1", "AREALSKU", 100, None, repo, session)
+    
+    with pytest.raises(services.InvalidSku, match="Invalid sku NONEXISTENTSKU"): 
+        services.allocate("o1", "NONEXISTENTSKU", 10, repo, FakeSession())
+
+
+@pytest.mark.usefixtures('postgres_db') 
+@pytest.mark.usefixtures('restart_api')
+def test_happy_path_returns_201_and_allocated_batch():
+    sku, othersku = random_sku(), random_sku('other')
+
+    earlybatch = random_batchref(1)
+    laterbatch = random_batchref(2)
+    otherbatch = random_batchref(3)
+
+    post_to_add_batch(laterbatch, sku, 100, '2011-01-02')
+    post_to_add_batch(earlybatch, sku, 100, '2011-01-01')
+    post_to_add_batch(otherbatch, othersku, 100, None)
+
+    data = {'orderid': random_orderid(), 'sku': sku, 'qty': 3}
+    url = config.get_api_url()
+    r = requests.post(f'{url}/allocate', json=data)
+
+    assert r.status_code == 201
+    assert r.json()['batchref'] == earlybatch
